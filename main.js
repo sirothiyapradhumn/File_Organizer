00384ed582757f837402cfcf2288e2f4a23aceae
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-03-12 21:46:13 +0530

    initial structure

---
+++ main.js
@@ -0,0 +1,12 @@
+// entry point of my command line
+
+let inputArr = process.argv.slice(2);
+let command = inputArr[0];
+switch (command) {
+    case "tree" : 
+
+    case "organise" :
+
+    case "help" :
+
+}
