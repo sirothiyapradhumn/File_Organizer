f66e8b219772f2cf060372ffa9ee670ec0a1a767
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-03-12 22:17:43 +0530

    help CLI complete

---
+++ commands/help.js
@@ -0,0 +1,13 @@
+function help() {
+    console.log(`
+        These are some myCLI commands used in various situations:
+
+            1. node main.js tree <path>
+            2. node main.js organize <path>
+            3. node maiin.js help
+    `);
+}
+
+module.exports = {
+    helpFunc: help
+}
--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 // entry point of my command line
+let helpFunc = require("./commands/help");
 
 let inputArr = process.argv.slice(2);
 let command = inputArr[0];
 switch (command) {
     case "tree" : 
-
-    case "organise" :
-
+        console.log("tree function call");
+        break;
+    case "organize" :
+        console.log("organize function call");
+        break;
     case "help" :
-
+        helpFunc.helpFunc();
+        break;
+    default:
+        console.log("command not recognized :/");
+        break;
 }
