79e08b117288695c3fe420b9d3f84754886bac96
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-03-20 21:36:16 +0530

    Organize CLI complete

--- commands/organize.js
+++ commands/organize.js
@@ -9,16 +9,16 @@ let types = {
     images: ['png','jpg','jpeg']
 }
 
-function organize(scrPath) {
-    //1. to check if scrPath is present
-    if(scrPath == undefined) {
+function organize(srcPath) {
+    //1. to check if srcPath is present
+    if(srcPath == undefined) {
         //The process.cwd() method returns the current working directory of the Node.js process.
-        scrPath = process.cwd();
-        //console.log(scrPath);
+        srcPath = process.cwd();
+        //console.log(srcPath);
     }
 
     //2. to create a directory -> organized_files
-    let organizedFiles = path.join(scrPath, "organized_files");
+    let organizedFiles = path.join(srcPath, "organized_files");
     //console.log(organizedFiles);
 
     // organizedFiles naam ka folder exits nhi krta to ek folder bana do warna rhne do
@@ -27,21 +27,73 @@ function organize(scrPath) {
     }
     else console.log("Folder already exists");
 
-    //3. scan the entire scrPath(download folder in this case)
+    //3. scan the entire srcPath(download folder in this case)
                     //Reads the contents of the directory. -> read the names of files present in directory
-    let allFiles = fs.readdirSync(scrPath);
-    console.log(allFiles);
+    let allFiles = fs.readdirSync(srcPath);
+    //console.log(allFiles);
 
 
     //4. travers over the files and classify them on the basis of their extension (.pdf, .mp3)
     for(let i = 0; i<allFiles.length; i++){
         //let extention = allFiles[i].split(".")[1];
         //alternte
-        let extention = path.extname(allFiles[i]);
-        console.log(extention);
+        //let extention = path.extname(allFiles[i]);
+        let fullPathOfFile = path.join(srcPath, allFiles[i]);
+        //console.log(fullPathOfFile);
+        //1. check if it is file or folder 
+        //lstatsync gives information regarding the link provided
+        let isThisAFile = fs.lstatSync(fullPathOfFile).isFile();
+        if(isThisAFile) {
+            //1.1 get extention name
+            let extention = path.extname(allFiles[i]).split(".")[1];
+            //console.log(extention);
+            //1.2 get folder name from extension
+            let folderName = getFolderName(extention); //archives
+            //console.log(folderName);
+            //1.3 copy from src folder (srcPath) and paste in dest folder (folder_name e.g. document, media etc)
+                    //copy      kya copy kro    paste 
+            copyFileToDest( srcPath, fullPathOfFile, folderName);
+            }
+        //console.log(extention);
     }
 
 }
 
-let scrPath = "F:\\FileOrganizer\\download";
-organize(scrPath);
+function getFolderName(extention) {
+  
+    //magic 
+    for(let key in types) {
+        //console.log(key);
+        for(let i = 0; i<types[key].length; i++){
+            if(types[key][i] == extention){
+                return key;
+            }
+        }
+    }
+    //edge case ->  koi type ka extention match nhi kar raha to miscellaneous folder mai chal jayega file 
+    return "miscellaneous";
+}
+
+function copyFileToDest(srcPath, fullPathOfFile, folderName){
+    //1. folderName ka paath banana hai
+    let destFolderPath = path.join(srcPath, "organized_files", folderName); // ........./downloads/organized_files/folderName(archives, media , etc)
+    //2 check folder if exits, if it does not , then make folder 
+    if(!fs.existsSync(destFolderPath)){
+        fs.mkdirSync(destFolderPath);
+    }
+    //3 copy file from src folder to dest folder 
+                      //basename -> Return the last portion of a path
+    let fileName = path.basename(fullPathOfFile); // abc.zip
+    let destFileName = path.join(destFolderPath, fileName);
+    fs.copyFileSync(fullPathOfFile, destFileName);
+}
+  
+
+// commenting because they are call from main.js
+// let srcPath = "F:\\FileOrganizer\\download";
+// organize(srcPath);
+
+
+module.exports = {
+    organize:organize
+}
--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // entry point of my command line
 let help = require("./commands/help");
-
+let organizeFun = require("./commands/organize");
 let inputArr = process.argv.slice(2);
 let command = inputArr[0];
+let path = inputArr[1];
 switch (command) {
     case "tree" : 
         console.log("tree function call");
         break;
     case "organize" :
-        console.log("organize function call");
+        organizeFun.organize(path);
         break;
     case "help" :
         help.helpFunc();
